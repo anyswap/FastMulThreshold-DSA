e522097c856145f1e4ca97a1e89d508ec1786221
haijuncai <yao63.com@163.com>
2021-10-11 00:22:07 +0800

    return when open sign result log file

--- cmd/gsmpc-client/main.go
+++ cmd/gsmpc-client/main.go
@@ -883,7 +883,7 @@ func PrintSignResultToLocalFile() {
     
     logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
     if err != nil {
-	    panic(err)
+	   return 
     }
     log.SetOutput(logFile) // 将文件设置为log输出的文件
     //log.SetPrefix("[Sign]")
