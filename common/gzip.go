f0d95746a30a141dc4e425ce620b419d97387d6f
xiaoming <1406629722@qq.com>
2023-05-09 22:43:29 +0800

    add gzip util file

---
+++ common/gzip.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"compress/gzip"
+	"io"
+)
+
+func Compress(writer io.Writer, reader io.Reader) error {
+	w := gzip.NewWriter(writer)
+	_, err := io.Copy(w, reader)
+	if err != nil {
+		return err
+	}
+	if err = w.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Decompress(writer io.Writer, reader io.Reader) error {
+	gzipReader, err := gzip.NewReader(reader)
+	defer gzipReader.Close()
+	_, err = io.Copy(writer, gzipReader)
+	if err != nil {
+		return err
+	}
+	return nil
+}
